3a1263123c3270d2abb7d66f2a0bf8964ad743b9
MaximOkhri <112479231+MaximOkhri@users.noreply.github.com>
2024-01-10 15:41:51 +0200

    Update Third_test.java

--- src/Third_test.java
+++ src/Third_test.java
@@ -6,7 +6,7 @@ public class Third_test {
         for (int i = 0; i < arr_for_test.length; i++) {
             arr_for_test[i] = ((int)(Math.random() * 100));
         }
-        System.out.println("Сгенерирован числовой массив:" + Arrays.toString(arr_for_test));
+        System.out.println("Сгенерирован числовой массив: " + Arrays.toString(arr_for_test));
         System.out.print("Список чисел кратных 3: ");
         for (int number : arr_for_test){
             if (number != 0 && number % 3 == 0){
