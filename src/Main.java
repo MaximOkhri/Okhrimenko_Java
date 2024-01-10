90861798bdb76a9ef1f01748c64ed10e3cefe6cc
MaximOkhri <112479231+MaximOkhri@users.noreply.github.com>
2024-01-10 15:40:40 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -8,26 +8,26 @@ public class Main {
 
         while (choice.equals("y")) {
 
-            System.out.print("Введите номер задания от 1 до 3:");
+            System.out.print("Введите номер задания от 1 до 3: ");
             Scanner choice_scanner = new Scanner(System.in);
             int choose_test = choice_scanner.nextInt();
 
             if (choose_test == 1) {
                 First_test number_test = new First_test();
                 number_test.first_test();
-                System.out.print("Введите 'y' для продолжения, 'n' чтобы выйти:");
+                System.out.print("Введите 'y' для продолжения, 'n' чтобы выйти: ");
                 Scanner next_choice_scanner = new Scanner(System.in);
                 choice = next_choice_scanner.nextLine();
             } else if (choose_test == 2) {
                 Second_test name_test = new Second_test();
                 name_test.second_test();
-                System.out.print("Введите 'y' для продолжения, 'n' чтобы выйти:");
+                System.out.print("Введите 'y' для продолжения, 'n' чтобы выйти: ");
                 Scanner next_choice_scanner = new Scanner(System.in);
                 choice = next_choice_scanner.nextLine();
             } else if (choose_test == 3) {
                 Third_test array_test = new Third_test();
                 array_test.third_test();
-                System.out.print("\nВведите 'y' для продолжения, 'n' чтобы выйти:");
+                System.out.print("\nВведите 'y' для продолжения, 'n' чтобы выйти: ");
                 Scanner next_choice_scanner = new Scanner(System.in);
                 choice = next_choice_scanner.nextLine();
             }
